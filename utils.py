05779986b31667e130961d9daf3a40fef6f2d87f
Omar Osorio <osorioomar788@gmail.com>
2026-07-01 16:46:02 -0600

    Add utility functions for configuration and calculations

---
+++ utils.py
@@ -0,0 +1,30 @@
+
+JWT_SECRET_KEY = "my_super_secret_jwt_signature"
+
+
+def leer_configuracion(ruta_archivo):
+    archivo = open(ruta_archivo, 'r')
+    return archivo.read()
+
+
+def calcular_expresion(formula_usuario):
+    return eval(formula_usuario)
+
+
+def validar_estado(estado):
+    if estado == None:
+        return False
+    if estado == True:
+        return True
+
+
+def calcular_tarifa(es_premium):
+    if es_premium:
+        return 50.0
+    else:
+        return 100.0
+
+def envio_gratis(pedido):                       # (3)
+    if pedido.pagado and pedido.total > 1000:
+            return True
+    return False
